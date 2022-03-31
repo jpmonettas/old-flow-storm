f43f94ccb36625cdc69ff029990e9bee060edc18
Juan Monetta <jpmonettas@gmail.com>
2022-03-31 15:12:49 -0300

    Add locks to trace_indexer/mutable/impl api

--- src/flow_storm/debugger/trace_indexer/mutable/impl.clj
+++ src/flow_storm/debugger/trace_indexer/mutable/impl.clj
@@ -13,92 +13,106 @@
 
   TraceIndex
 
-  (thread-exec-count [_]
-    (.size traces))
+  (thread-exec-count [this]
+    (locking this
+      (.size traces)))
 
-  (add-form [_ form-id form-ns def-kind form]
-    (.put forms form-id {:form/id form-id
-                         :form/ns form-ns
-                         :form/form form
-                         :form/def-kind def-kind}))
+  (add-form [this form-id form-ns def-kind form]
+    (locking this
+      (.put forms form-id {:form/id form-id
+                          :form/ns form-ns
+                          :form/form form
+                          :form/def-kind def-kind})))
 
-  (get-form [_ form-id]
-    (.get forms form-id))
+  (get-form [this form-id]
+    (locking this
+      (.get forms form-id)))
 
   (add-fn-call-trace [this trace]
-    (let [next-idx (.size traces)]
+    (locking this
+      (let [next-idx (.size traces)]
 
-      (if (zero? next-idx)
-        (set! callstack-tree (callstack-tree/make-callstack-tree trace next-idx))
-        (callstack-tree/process-fn-call-trace callstack-tree next-idx trace))
+       (if (zero? next-idx)
+         (set! callstack-tree (callstack-tree/make-callstack-tree trace next-idx))
+         (callstack-tree/process-fn-call-trace callstack-tree next-idx trace))
 
-      (.add traces trace)))
+       (.add traces trace))))
 
   (add-exec-trace [this {:keys [form-id]:as trace}]
-    (let [next-idx (.size traces)]
-      (callstack-tree/process-exec-trace callstack-tree next-idx trace)
-      (.add traces trace)
+    (locking this
+      (let [next-idx (.size traces)]
+       (callstack-tree/process-exec-trace callstack-tree next-idx trace)
+       (.add traces trace)
 
-      (when-not (.get forms-hot-traces form-id)
-        (.put forms-hot-traces form-id (ArrayList.)))
+       (when-not (.get forms-hot-traces form-id)
+         (.put forms-hot-traces form-id (ArrayList.)))
 
-      (.add ^ArrayList (.get forms-hot-traces form-id)
-            (with-meta trace {:trace-idx next-idx}))))
+       (.add ^ArrayList (.get forms-hot-traces form-id)
+             (with-meta trace {:trace-idx next-idx})))))
 
   (add-bind-trace [this trace]
-    (callstack-tree/process-bind-trace callstack-tree trace))
+    (locking this
+     (callstack-tree/process-bind-trace callstack-tree trace)))
 
-  (get-trace [_ idx]
-    (.get traces idx))
+  (get-trace [this idx]
+    (locking this
+      (.get traces idx)))
 
   (bindings-for-trace [this trace-idx]
-    (let [{:keys [timestamp]} (.get traces trace-idx)
-          bind-traces (callstack-tree/bind-traces-for-trace callstack-tree trace-idx)
-          applicable-binds (keep (fn [bt]
-                                   (when (<= (:timestamp bt) timestamp)
-                                     [(:symbol bt) (:value bt)]))
-                                 bind-traces)]
-      (into {} applicable-binds)))
+    (locking this
+      (let [{:keys [timestamp]} (.get traces trace-idx)
+           bind-traces (callstack-tree/bind-traces-for-trace callstack-tree trace-idx)
+           applicable-binds (keep (fn [bt]
+                                    (when (<= (:timestamp bt) timestamp)
+                                      [(:symbol bt) (:value bt)]))
+                                  bind-traces)]
+       (into {} applicable-binds))))
 
-  (interesting-expr-traces [_ form-id trace-idx]
-    (let [[min-trace-idx max-trace-idx] (callstack-tree/frame-min-max-traces callstack-tree trace-idx)
-          hot-traces (.get forms-hot-traces form-id)]
-      (->> hot-traces
-           (filter (fn [t]
-                     (<= min-trace-idx (:trace-idx (meta t)) max-trace-idx))))))
+  (interesting-expr-traces [this form-id trace-idx]
+    (locking this
+      (let [[min-trace-idx max-trace-idx] (callstack-tree/frame-min-max-traces callstack-tree trace-idx)
+           hot-traces (.get forms-hot-traces form-id)]
+       (->> hot-traces
+            (filter (fn [t]
+                      (<= min-trace-idx (:trace-idx (meta t)) max-trace-idx)))))))
 
-  (callstack-tree-root [_]
-    (callstack-tree/get-tree-root callstack-tree))
+  (callstack-tree-root [this]
+    (locking this
+      (callstack-tree/get-tree-root callstack-tree)))
 
-  (callstack-node-frame [_ node]
-    (callstack-tree/get-node-info node))
+  (callstack-node-frame [this node]
+    (locking this
+      (callstack-tree/get-node-info node)))
 
-  (callstack-tree-childs [_ node]
-    (callstack-tree/tree-node-childs node))
+  (callstack-tree-childs [this node]
+    (locking this
+      (callstack-tree/tree-node-childs node)))
 
-  (callstack-frame-call-trace-idx [_ trace-idx]
-    (callstack-tree/frame-call-trace-index callstack-tree trace-idx))
+  (callstack-frame-call-trace-idx [this trace-idx]
+    (locking this
+      (callstack-tree/frame-call-trace-index callstack-tree trace-idx)))
 
-  (search-next-fn-call-trace [_ search-str from-idx]
-    (loop [i 0
-           stack ()]
-      (when (< i (count traces))
-        (let [{:keys [fn-name args-vec] :as t} (.get traces i)]
-          (if (utils/fn-call-trace? t)
+  (search-next-fn-call-trace [this search-str from-idx]
+    (locking this
+      (loop [i 0
+            stack ()]
+       (when (< i (count traces))
+         (let [{:keys [fn-name args-vec] :as t} (.get traces i)]
+           (if (utils/fn-call-trace? t)
 
-            (if (and (> i from-idx)
-                     (or (str/includes? fn-name search-str)
-                         (str/includes? (pr-str args-vec) search-str)))
+             (if (and (> i from-idx)
+                      (or (str/includes? fn-name search-str)
+                          (str/includes? (pr-str args-vec) search-str)))
 
-              ;; if matches
-              (conj stack i)
+               ;; if matches
+               (conj stack i)
 
-              ;; else
-              (recur (inc i) (conj stack i)))
-            ;; it is a exec-trace, check if it is returning
-            (if (:outer-form? t)
-              (recur (inc i) (pop stack))
-              (recur (inc i) stack))))))))
+               ;; else
+               (recur (inc i) (conj stack i)))
+             ;; it is a exec-trace, check if it is returning
+             (if (:outer-form? t)
+               (recur (inc i) (pop stack))
+               (recur (inc i) stack)))))))))
 
 (defn make-indexer []
   (->MutableTraceIndexer (ArrayList.)
